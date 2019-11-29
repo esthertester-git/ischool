879d50c303ef966f56e83d4768969e691a1e9156
Esther Duncan <esther@magnusandmaeve.com>
2019-11-29 16:08:50 -0500

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -39,6 +39,9 @@ async function run() {
 tfvis.show.modelSummary({name: 'Model Architecture'}, model);
   
 await train(model, data);
+
+await showAccuracy(model, data);
+await showConfusion(model, data);
   
 }
 
